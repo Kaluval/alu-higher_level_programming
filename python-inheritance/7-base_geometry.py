a82d6abfd5002bb7b458458ce7d394e4a11cd931
Valentine <v.kalu@alustudent.com>
2022-11-18 21:21:23 +0000

    7th

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-'''Public instance method: def area(self):'''
+"""Inherits BaseGeometry class"""
 
 
-class BaseGeometry:
-    '''Base class'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle."""
+
+    def __init__(self, size):
+        """set the values"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-        '''raise an exception for now'''
-        raise Exception("area() is not implemented")
+        """Area of Square"""
+        return self.__size ** 2
 
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-            
+    def __str__(self):
+        '''Returns [Square] '''
+        return str("[Square] {}/{}".format(self.__size, self.__size))
