9496b7e8eb073cbcfc0553d81ba750cecdbd5f27
Kaluval <v.kalu@alustudent.com>
2022-11-25 18:27:42 +0200

    remove

--- 1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" script that takes in a URL, sends a request to the URL and displays"""
-import urllib.request
-import sys
-
-
-if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as res:
-        print(res.info()['X-Request-Id'])
