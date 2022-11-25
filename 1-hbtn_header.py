11451af4264ee93eb1e12e4cf87e03dfe730582b
Kaluval <v.kalu@alustudent.com>
2022-11-25 18:04:32 +0200

    header

--- 1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" script that takes in a URL, sends a request to the URL and disolays"""
-import urllib.request
-import sys
-
-
-if __name__== "__main__":
-    with urlib.request.urlopen(sys.argv[1]) as res:
-        print(res.info()['X--Request-Id'])
