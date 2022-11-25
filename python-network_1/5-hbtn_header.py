2a08c382b6f17b577cfc52883c7fffd0062c594e
Valentine <v.kalu@alustudent.com>
2022-11-25 20:35:01 +0000

    66

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Check status"""
+'''sends a request and displays the value of a variable'''
 import requests
 import sys
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    r = requests.get(url)
-    print(r.headers.get('x-request-id'))
-
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
