efe9694f34c5b43f1d63df82302dd691492af1c3
Valentine <v.kalu@alustudent.com>
2022-11-25 20:39:16 +0000

    proper

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-'''sends a request and displays the value of a variable'''
-import requests
+# python script that takes in a URL,
+# sends a request to the URL and displays the value of the,
+# variable X-Request-Id in the response header
+"""
+    send a request to the URL & display value of the,
+    variable X-Request-Id in the response header
+"""
 import sys
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
