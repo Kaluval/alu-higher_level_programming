b43740869350e1dede771ffbcf479890e1884f5e
Valentine <v.kalu@alustudent.com>
2022-11-25 20:30:24 +0000

    force

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
--- id cant be null 
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    name VARCHAR(256)
-);
+-- a script that creates the table force_name
+-- on your MySQL server
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
