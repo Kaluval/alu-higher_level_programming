0afeb2c82abeb9172a4b567d627a1bbe524fcd99
Valentine <v.kalu@alustudent.com>
2022-11-25 20:44:41 +0000

    error

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-# python script that takes in a URL,
-# sends a request to the URL and displays the value of the,
-# variable X-Request-Id in the response header
-"""
-    send a request to the URL & display value of the,
-    variable X-Request-Id in the response header
-"""
-import sys
+'''sends a request and displays the value of a variable'''
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
