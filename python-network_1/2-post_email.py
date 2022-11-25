2d740301695bed17bb246fe93cd75ad5af591c10
Kaluval <v.kalu@alustudent.com>
2022-11-25 18:54:47 +0000

    correction code 2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """Send POST"""
 import urllib.request
@@ -13,20 +12,3 @@ if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as res:
         print(res.read().decode('utf-8'))
-=======
-#!/usr/bin/python3
-"""Send POST"""
-import urllib.request
-import urllib.parse
-import sys
-
-
-if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
-        
->>>>>>> 791e1c6f28ca08dc827c57208d1108bdb220cc1d
